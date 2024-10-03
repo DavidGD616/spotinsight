de5e50c1c7d5c7a7c8f70a00ec42f6035390e9c9
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-10-02 18:07:24 -0700

    OAuth tokens passed to React app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
+const backendUrl = 'http://localhost:8888'
+
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
+  }, []);
 
   return (
     <div className='bg-red-900'>
-      Hello WOrld
+      <a href={backendUrl + "/login"}>
+        Log in to Spotify
+      </a>
     </div>
   )
 }
