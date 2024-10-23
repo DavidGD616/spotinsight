514fe8030ab6311baa4c7ede2d3c044e9df1f5ac
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-10-22 19:10:29 -0700

    User

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,46 +1,52 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import {getUserInfo, logout } from '../spotify';
-import { catchErrors } from "../../utils";
+import { getUser, logout } from "../spotify";
+import { catchErrors } from "../utils";
 
 const User = () => {
-    const [user, setUser] = useState(null);
-    const [followedArtists, setFollowedArtists] = useState(null);
-    const [playlists, setPlaylists] = useState(null);
-    const [topArtists, setTopArtists] = useState(null);
-    const [topTracks, setTopTracks] = useState(null);
+  const [user, setUser] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const { user, followedArtists, playlists, topArtists, topTracks } = await getUserInfo();
-            setUser(user);
-            setFollowedArtists(followedArtists);
-            setPlaylists(playlists);
-            setTopArtists(topArtists);
-            setTopTracks(topTracks);
-          };
-          catchErrors(fetchData());
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const userProfile = await getUser();
+      setUser(userProfile.data);
+    };
 
-    const totalPlaylists = playlists ? playlists.total : 0;
+    catchErrors(fetchData());
+  }, []);
 
-    return (
-        <React.Fragment>
-            {user ? (
-                <div>
-                    {user.images.length > 0 ? (
-                        <img src={user.images[0].url} alt="avatar" />
-                    ) : (
-                        <div>
-                            No Avatar
-                        </div>
-                    )}
-                </div>
+  return (
+    <>
+      {user ? (
+        <>
+          <div className="w-[150px] h-[150px]">
+            {user.images.length > 0 ? (
+              <img
+                className="rounded-full"
+                src={user.images[0].url}
+                alt="avatar"
+              />
             ) : (
-                <div>Loading ...</div>
+              <div>NoIconUser</div>
             )}
-        </React.Fragment>
-    )
-}
+          </div>
+          <a className="hover:text-[#1ed760] focus:text-[#1ed760]">
+            <h1 className="text-[8vw] sm:text-[50px] font-bold mt-5">
+              {user.display_name}
+            </h1>
+          </a>
+          <div>
+            <div>
+                <div></div>
+                <p></p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>Loading ....</div>
+      )}
+    </>
+  );
+};
 
 export default User;
