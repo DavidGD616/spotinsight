284c9b57fe0aeefe8ca818a3fa756fca072c3f88
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-11-01 21:08:32 -0700

    Nav Playlists  Button

--- client/src/assets/index.jsx
+++ client/src/assets/index.jsx
@@ -3,3 +3,4 @@ export { default as UserIcon } from './UserIcon';
 export { default as MicrophoneIcon } from './MicrophoneIcon';
 export { default as MusicIcon } from './MusicIcon';
 export { default as TimeIcon } from './TimeIcon';
+export { default as PlaylistIcon } from './PlaylistIcon';
