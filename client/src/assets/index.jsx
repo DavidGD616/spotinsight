cda7523f2774e758d64e1a3286f293ca547f0683
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-11-01 20:56:33 -0700

    Nav TopTracks Button

--- client/src/assets/index.jsx
+++ client/src/assets/index.jsx
@@ -1,3 +1,4 @@
 export { default as IconSpotInsight } from './spotinsightLogo';
 export { default as UserIcon } from './UserIcon';
 export { default as MicrophoneIcon } from './MicrophoneIcon';
+export { default as MusicIcon } from './MusicIcon';
