05cfd798cfcfafc998dddf5d063f1507eb529ec8
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-12-04 17:19:11 -0800

    chips updated for mobiles

--- client/src/pages/GenrePage.jsx
+++ client/src/pages/GenrePage.jsx
@@ -20,32 +20,36 @@ const GenrePage = () => {
         <ul className="mt-[50px]">
           {artists.map((artist, index) => (
             <li key={index}>
-              <Link className="grid grid-cols-[auto_1fr] items-center mb-[20px] md:mb-[20px]">
-                <div>
-                  <div className="relative mr-[16px]">
-                    <img
-                      src={artist.images[0].url}
-                      alt={artist.name}
-                      className="rounded-full h-16 w-16 object-cover"
-                    />
-                  </div>
-                </div>
-                <div className="grid grid-cols-[1fr_max-content] gap-[10px]">
-                  <span className="overflow-hidden text-ellipsis whitespace-nowrap pr-[1px]">
-                    {artist.name}
-                    <div className="overflow-hidden text-ellipsis whitespace-nowrap pr-[1px] text-[#9B9B9B] text-[14px] mt-[3px]">
-                        {artist.genres.map((genre, index) => (
-                        <span
-                            key={index}
-                            className="bg-gray-700 text-gray-300 text-xs font-medium py-1 px-2 rounded-full shadow-sm mr-2"
-                        >
-                            {genre}
-                        </span>
-                        ))}
+              <div className="grid grid-cols-[auto_1fr] items-center mb-[20px] md:mb-[20px]">
+                <Link>
+                  <div>
+                    <div className="relative mr-[16px]">
+                      <img
+                        src={artist.images[0].url}
+                        alt={artist.name}
+                        className="rounded-full h-16 w-16 object-cover"
+                      />
                     </div>
-                  </span>
-                </div>
-              </Link>
+                  </div>
+                  </Link>
+                  <div className="grid grid-cols-[1fr_max-content] gap-[10px]">
+                    <span className="overflow-hidden text-ellipsis whitespace-nowrap pr-[1px]">
+                      <Link>
+                        {artist.name}
+                      </Link>
+                      <div className="flex flex-wrap gap-2 overflow-hidden text-ellipsis whitespace-normal pr-[1px] text-[#9B9B9B] text-[14px] mt-[3px]">
+                          {artist.genres.map((genre, index) => (
+                          <span
+                              key={index}
+                              className="bg-gray-700 text-gray-300 text-xs font-medium py-1 px-2 rounded-full shadow-sm mr-2"
+                          >
+                              {genre}
+                          </span>
+                          ))}
+                      </div>
+                    </span>
+                  </div>
+              </div>
             </li>
           ))}
         </ul>
