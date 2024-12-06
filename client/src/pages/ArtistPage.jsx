ddd244e5240ada5d873731a8fca853d48d39d20e
DavidGD616 <davidguerrerodiaz12@gmail.com>
2024-12-05 17:47:06 -0800

    artist page format

--- client/src/pages/ArtistPage.jsx
+++ client/src/pages/ArtistPage.jsx
@@ -7,7 +7,7 @@ const ArtistPage = () => {
   const { artistId } = useParams();
   const { artist } = useArtist(artistId);
   const { albums } = useArtistAlbums(artistId);
-  console.log(albums)
+  console.log(albums);
 
   return (
     <Main>
@@ -20,7 +20,9 @@ const ArtistPage = () => {
               className="w-64 h-64 rounded-lg object-cover mb-4"
             />
             <h1 className="text-3xl font-bold">{artist.name}</h1>
-            <p className="text-gray-400">{artist.type.charAt(0).toUpperCase() + artist.type.slice(1)}</p>
+            <p className="text-gray-400">
+              {artist.type.charAt(0).toUpperCase() + artist.type.slice(1)}
+            </p>
           </div>
           <div className="mt-8 flex justify-center gap-4">
             <div className="bg-[#282828] rounded-lg p-4 flex flex-col items-center w-full md:w-48 shadow-md">
@@ -38,24 +40,25 @@ const ArtistPage = () => {
           </div>
           {/* Albums Section */}
           <div className="mt-8">
-  <h2 className="text-2xl font-bold mb-4">Albums</h2>
-  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-    {albums.map((album) => (
-      <div key={album.id} className="text-center">
-        <img
-          src={album.images?.[0]?.url}
-          alt={album.name}
-          className="w-full h-25 object-cover rounded-lg"
-        />
-        <p className="text-white text-sm mt-2 font-bold">{album.name}</p>
-        <p className="text-gray-400 text-xs">
-          {new Date(album.release_date).toISOString().split("T")[0]}
-        </p>
-      </div>
-    ))}
-  </div>
-</div>
-
+            <h2 className="text-2xl font-bold mb-4">Albums</h2>
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+              {albums.map((album) => (
+                <div key={album.id} className="text-center">
+                  <img
+                    src={album.images?.[0]?.url}
+                    alt={album.name}
+                    className="w-full h-25 object-cover rounded-lg"
+                  />
+                  <p className="text-white text-sm mt-2 font-bold">
+                    {album.name}
+                  </p>
+                  <p className="text-gray-400 text-xs">
+                    {new Date(album.release_date).toISOString().split("T")[0]}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
         </>
       ) : (
         <div>Loading...</div>
